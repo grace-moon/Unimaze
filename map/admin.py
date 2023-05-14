5dc5461edfe5b531e3aeb320e4e5eada312b1a23
grace-moon <grace.08de98@gmail.com>
2023-05-15 02:15:44 +0900

    change models.py and ather things'

--- map/admin.py
+++ map/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from .models import Unimaze_Post, Direcrions_Post, Univ_Contacts, HRC, MAC_L, MAC_M, TTD, EP, CH, AS, SH, EN, HS, BS, JL, EE, OM, AD, HC, TSD, MM, TTA, TFD, FS, IAC
+from .models import Unimaze_Post, Univ_Contacts,Unimaze_map
 
 
 class UnimazePostAdmin(admin.ModelAdmin):
     list_display = ['title', 'text',]
 admin.site.register(Unimaze_Post, UnimazePostAdmin)
-
-
-class DirectionsPostAdmin(admin.ModelAdmin):
-    list_display = ['building_name', 'building_num', 'floor',]
-admin.site.register(Direcrions_Post, DirectionsPostAdmin)
-
 
 class UnivContactsAdmin(admin.ModelAdmin):
     list_display = ['department', 'name', 'task',]
@@ -19,94 +13,7 @@ admin.site.register(Univ_Contacts, UnivContactsAdmin)
 
 
 #3D map admin
-class TTA_Admin(admin.ModelAdmin):
+class Map_Admin(admin.ModelAdmin):
     list_display = ['building_name', 'floor',]
-admin.site.register(TTA, TTA_Admin)
-
-class FS_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(FS, FS_Admin)
-
-class HC_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(HC, HC_Admin)
-
-class HS_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(HS, HS_Admin)
-
-class IAC_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(IAC, IAC_Admin)
-
-class TFD_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(TFD, TFD_Admin)
-
-class MM_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(MM, MM_Admin)
-
-class TSD_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(TSD, TSD_Admin)
-
-class AD_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(AD, AD_Admin)
-
-class OM_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(OM, OM_Admin)
-
-class EE_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(EE, EE_Admin)
-
-class JL_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(JL, JL_Admin)
-
-class BS_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(BS, BS_Admin)
-
-class EN_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(EN, EN_Admin)
-
-class SH_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(SH, SH_Admin)
-
-class AS_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(AS, AS_Admin)
-
-class CH_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(CH, CH_Admin)
-
-class EP_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(EP, EP_Admin)
-
-class TTD_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(TTD, TTD_Admin)
-
-class MAC_M_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(MAC_M, MAC_M_Admin)
-
-class MAC_L_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(MAC_L, MAC_L_Admin)
-
-class HRC_Admin(admin.ModelAdmin):
-    list_display = ['building_name', 'floor',]
-admin.site.register(HRC, HRC_Admin)
-
-
-
+admin.site.register(Unimaze_map, Map_Admin)
 
--- map/views.py
+++ map/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Unimaze_Post, Direcrions_Post, Univ_Contacts, HRC, MAC_L, MAC_M, TTD, EP, CH, AS, SH, EN, HS, BS, JL, EE, OM, AD, HC, TSD, MM, TTA, TFD, FS, IAC
+from .models import Unimaze_Post,Unimaze_map, Univ_Contacts
 from .forms import PostForm
 from django.utils import timezone
 from django.shortcuts import redirect
@@ -18,135 +18,20 @@ def post_detail(request, pk):
     return render(request, '../templates/map/post_detail.html', {'post':post})
 
 
-
-
-
-def direcions_detail(request, pk):
-    post = get_object_or_404(Direcrions_Post, pk=pk)
-    return render(request, '../templates/map/directions_detail.html', {'post':post})
-
-def contracts_detail(request, pk):
+def contacts_detail(request, pk):
     contract = get_object_or_404(Univ_Contacts, pk=pk)
     return render(request, '../templates/map/contracts_detail.html', {'contract':contract})
 
 
-#건물별 views
-def HRC_map(request, pk):
-    vectary_map = get_object_or_404(HRC, pk=pk)
-    map_list = HRC.objects.all().order_by('floor')
-    return render(request, '../templates/map/HRC_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def MAC_L_map(request, pk):
-    vectary_map = get_object_or_404(MAC_L, pk=pk)
-    map_list = MAC_L.objects.all().order_by('floor')
-    return render(request, '../templates/map/MAC_L_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def MAC_M_map(request, pk):
-    vectary_map = get_object_or_404(MAC_M, pk=pk)
-    map_list = MAC_M.objects.all().order_by('floor')
-    return render(request, '../templates/map/MAC_M_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def TTD_map(request, pk):
-    vectary_map = get_object_or_404(TTD, pk=pk)
-    map_list = TTD.objects.all().order_by('floor')
-    return render(request, '../templates/map/TTD_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def EP_map(request, pk):
-    vectary_map = get_object_or_404(EP, pk=pk)
-    map_list = EP.objects.all().order_by('floor')
-    return render(request, '../templates/map/EP_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def CH_map(request, pk):
-    vectary_map = get_object_or_404(CH, pk=pk)
-    map_list = CH.objects.all().order_by('floor')
-    return render(request, '../templates/map/CH_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def AS_map(request, pk):
-    vectary_map = get_object_or_404(AS, pk=pk)
-    map_list = AS.objects.all().order_by('floor')
-    return render(request, '../templates/map/AS_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def SH_map(request, pk):
-    vectary_map = get_object_or_404(SH, pk=pk)
-    map_list = SH.objects.all().order_by('floor')
-    return render(request, '../templates/map/SH_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def EN_map(request, pk):
-    vectary_map = get_object_or_404(EN, pk=pk)
-    map_list = EN.objects.all().order_by('floor')
-    return render(request, '../templates/map/EN_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def HS_map(request, pk):
-    vectary_map = get_object_or_404(HS, pk=pk)
-    map_list = HS.objects.all().order_by('floor')
-    return render(request, '../templates/map/HS_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def BS_map(request, pk):
-    vectary_map = get_object_or_404(BS, pk=pk)
-    map_list = BS.objects.all().order_by('floor')
-    return render(request, '../templates/map/BS_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def JL_map(request, pk):
-    vectary_map = get_object_or_404(JL, pk=pk)
-    map_list = JL.objects.all().order_by('floor')
-    return render(request, '../templates/map/JL_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def EE_map(request, pk):
-    vectary_map = get_object_or_404(EE, pk=pk)
-    map_list = EE.objects.all().order_by('floor')
-    return render(request, '../templates/map/EE_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def OM_map(request, pk):
-    vectary_map = get_object_or_404(OM, pk=pk)
-    map_list = OM.objects.all().order_by('floor')
-    return render(request, '../templates/map/OM_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def AD_map(request, pk):
-    vectary_map = get_object_or_404(AD, pk=pk)
-    map_list = AD.objects.all().order_by('floor')
-    return render(request, '../templates/map/AD_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def HC_map(request, pk):
-    vectary_map = get_object_or_404(HC, pk=pk)
-    map_list = HC.objects.all().order_by('floor')
-    return render(request, '../templates/map/HC_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def TSD_map(request, pk):
-    vectary_map = get_object_or_404(TSD, pk=pk)
-    map_list = TSD.objects.all().order_by('floor')
-    return render(request, '../templates/map/TSD_map_detail.html',{'map':vectary_map,'list':map_list})
-
-def MM_map(request, pk):
-    vectary_map = get_object_or_404(MM, pk=pk)
-    map_list = MM.objects.all().order_by('floor')
-    return render(request, '../templates/map/MM_map_detail.html',{'map':vectary_map,'list':map_list})
-
 def TTA_map(request, pk):
-    vectary_map = get_object_or_404(TTA, pk=pk)
-    map_list = TTA.objects.all().order_by('published_date')
-    return render(request, '../templates/map/TTA_map_detail.html', {'map':vectary_map,'list':map_list})
-
-def TFD_map(request, pk):
-    vectary_map = get_object_or_404(TFD, pk=pk)
-    map_list = TFD.objects.all().order_by('floor')
-    return render(request, '../templates/map/TFD_map_detail.html', {'map':vectary_map,'list':map_list})
-
-def FS_map(request, pk):
-    vectary_map = get_object_or_404(FS, pk=pk)
-    map_list = FS.objects.all().order_by('floor')
-    return render(request, '../templates/map/FS_map_detail.html', {'map':vectary_map,'list':map_list})
-
-def IAC_map(request, pk):
-    vectary_map = get_object_or_404(IAC, pk=pk)
-    map_list = IAC.objects.all().order_by('floor')
-    return render(request, '../templates/map/IAC_map_detail.html', {'map':vectary_map,'list':map_list})
-
-
+    vectary_map = get_object_or_404(Unimaze_map, pk=pk)
+    map_list = Unimaze_map.objects.filter(building_num=22).order_by('published_date')
+    return render(request, '../templates/map/map_detail.html', {'map':vectary_map,'list':map_list})
 
 '''
 def result(request):
     query = request.GET['q']
-    posts = Unimaze_Post, Direcrions_Post, Univ_Contacts, HRC, MAC_L, MAC_M, TTD, EP, CH, AS, SH, EN, HS, BS, JL, EE, OM, AD, HC, TSD, MM, TTA, TFD, FS, IAC
+    posts = Unimaze_Post, Direcrions_Post, Univ_Contacts, HRC, MAC_L, MAC_M, TTD, EP, CH, AS, SH, EN, HS, BS, JL, EE, OM, AD, HC, TSD, MM| Map_detail, TFD, FS, IAC
         Post.objects.filter(title__contains=query) | Post.objects.filter(text__contains=query)
 #    if 'q' in request.GET:
 #        post = post.objects.all().filter(title__contains=query)| Q(description__contains=query)
@@ -168,3 +53,19 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'map/post_edit.html', {'form': form})
+
+
+def map_result(request):
+    query = request.GET['q']
+    posts = Unimaze_map.objects.filter(building_name__contains=query)|Unimaze_map.objects.filter(building_num__exact=22)|Unimaze_map.objects.filter(text__contains=query)
+#    if 'q' in request.GET:
+#        post = post.objects.all().filter(title__contains=query)| Q(description__contains=query)
+    return render(request, 'map/map_search_results.html', {'posts':posts})
+
+
+def contacts_result(request):
+    query = request.GET['q']
+    contacts = Univ_Contacts.objects.filter(department__contains=query) | Univ_Contacts.objects.filter(name=query) | Univ_Contacts.objects.filter(task__contains=query) #    if 'q' in request.GET:
+#        post = post.objects.all().filter(title__contains=query)| Q(description__contains=query)
+    return render(request, 'map/map_search_results.html', {'contacts':contacts})
+
